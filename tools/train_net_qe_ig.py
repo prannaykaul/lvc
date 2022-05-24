81739f1a777f81755c62db21a9b133cdb756be1b
Prannay Kaul <prannay.kaul@me.com>
2022-05-24 16:34:57 +0100

    missing training script

---
+++ tools/train_net_qe_ig.py
@@ -0,0 +1,225 @@
+"""
+Detection Training Script.
+
+This scripts reads a given config file and runs the training or evaluation.
+It is an entry point that is made to train standard models in FsDet.
+
+In order to let one script support training of many models,
+this script contains logic that are specific to these built-in models and
+therefore may not be suitable for your own project.
+For example, your research project perhaps only needs a single "evaluator".
+
+Therefore, we recommend you to use FsDet as an library and take
+this file as an example of how to use the library.
+You may want to write your own script with your datasets and other customizations.
+"""
+
+from fsdet.config import get_cfg, set_global_cfg
+from fsdet.engine import DefaultTrainer, default_argument_parser, default_setup
+
+import detectron2.utils.comm as comm
+import os
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.data import MetadataCatalog
+from detectron2.engine import launch
+from fsdet.evaluation import (
+    COCOEvaluator,
+    DatasetEvaluators,
+    LVISEvaluator,
+    PascalVOCDetectionEvaluator,
+    verify_results,
+    RPNEvaluator,
+    ImageContextEvaluator,
+    )
+
+from typing import Any, Dict, List, Set
+import torch
+from detectron2.config import CfgNode
+
+import resource
+from fsdet.data import (
+    build_detection_train_loader,
+    build_detection_train_mosaic_loader,
+    )
+from fsdet.data.dataset_mapper import DatasetMapperIgnore
+
+rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+resource.setrlimit(resource.RLIMIT_NOFILE, (rlimit[0], rlimit[1]))
+# resource.setrlimit(resource.RLIMIT_NOFILE, (8192, rlimit[1]))
+
+
+def build_clip_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimizer:
+    """
+    Build an optimizer from config.
+    """
+    from detectron2.solver.build import maybe_add_gradient_clipping
+    norm_module_types = (
+        torch.nn.BatchNorm1d,
+        torch.nn.BatchNorm2d,
+        torch.nn.BatchNorm3d,
+        torch.nn.SyncBatchNorm,
+        # NaiveSyncBatchNorm inherits from BatchNorm2d
+        torch.nn.GroupNorm,
+        torch.nn.InstanceNorm1d,
+        torch.nn.InstanceNorm2d,
+        torch.nn.InstanceNorm3d,
+        torch.nn.LayerNorm,
+        torch.nn.LocalResponseNorm,
+    )
+    params: List[Dict[str, Any]] = []
+    memo: Set[torch.nn.parameter.Parameter] = set()
+    for name, module in model.named_modules():
+        for key, value in module.named_parameters(recurse=False):
+            if not value.requires_grad:
+                continue
+            # Avoid duplicating parameters
+            if value in memo:
+                continue
+            memo.add(value)
+            if 'backbone.bottom_up' in name:
+                lr = cfg.SOLVER.CLIP_LR
+            else:
+                lr = cfg.SOLVER.BASE_LR
+            weight_decay = cfg.SOLVER.WEIGHT_DECAY
+            if isinstance(module, norm_module_types):
+                weight_decay = cfg.SOLVER.WEIGHT_DECAY_NORM
+            elif key == "bias":
+                # NOTE: unlike Detectron v1, we now default BIAS_LR_FACTOR to 1.0
+                # and WEIGHT_DECAY_BIAS to WEIGHT_DECAY so that bias optimizer
+                # hyperparameters are by default exactly the same as for regular
+                # weights.
+                lr = cfg.SOLVER.BASE_LR * cfg.SOLVER.BIAS_LR_FACTOR
+                weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
+            params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
+
+    optimizer = torch.optim.SGD(
+        params, cfg.SOLVER.BASE_LR, momentum=cfg.SOLVER.MOMENTUM, nesterov=cfg.SOLVER.NESTEROV
+    )
+    optimizer = maybe_add_gradient_clipping(cfg, optimizer)
+    return optimizer
+
+
+class Trainer(DefaultTrainer):
+    """
+    We use the "DefaultTrainer" which contains a number pre-defined logic for
+    standard training workflow. They may not work for you, especially if you
+    are working on a new research project. In that case you can use the cleaner
+    "SimpleTrainer", or write your own training loop.
+    """
+
+    @classmethod
+    def build_optimizer(cls, cfg, model):
+        from detectron2.solver import build_optimizer
+
+        if 'clip' in cfg.MODEL.WEIGHTS:
+            return build_clip_optimizer(cfg, model)
+        else:
+            return build_optimizer(cfg, model)
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        """
+        Create evaluator(s) for a given dataset.
+        This uses the special metadata "evaluator_type" associated with each builtin dataset.
+        For your own dataset, you can simply create an evaluator manually in your
+        script and do not have to worry about the hacky if-else logic here.
+        """
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        evaluator_list = []
+        if cfg.MODEL.META_ARCHITECTURE == 'ProposalNetwork':
+            evaluator_list.append(
+                RPNEvaluator(dataset_name, cfg, True, output_folder))
+            return DatasetEvaluators(evaluator_list)
+        elif cfg.MODEL.META_ARCHITECTURE == 'GeneralizedRCNN_Context' and cfg.MODEL.IMAGES_ONLY:
+            evaluator_list.append(
+                ImageContextEvaluator(dataset_name, cfg, True, output_folder))
+            return DatasetEvaluators(evaluator_list)
+        evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
+        if evaluator_type == "coco":
+            evaluator_list.append(
+                COCOEvaluator(dataset_name, cfg, True, output_folder)
+            )
+        if evaluator_type == "pascal_voc":
+            return PascalVOCDetectionEvaluator(dataset_name, output_folder)
+        if evaluator_type == "lvis":
+            return LVISEvaluator(dataset_name, cfg, True, output_folder)
+        if len(evaluator_list) == 0:
+            raise NotImplementedError(
+                "no Evaluator for the dataset {} with the type {}".format(
+                    dataset_name, evaluator_type
+                )
+            )
+        if len(evaluator_list) == 1:
+            return evaluator_list[0]
+        return DatasetEvaluators(evaluator_list)
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        """
+        Returns:
+            iterable
+
+        It now calls :func:`fsdet.data.build_detection_train_loader`.
+        Overwrite it if you'd like a different data loader.
+        """
+        if cfg.INPUT.MOSAIC:
+            return build_detection_train_mosaic_loader(cfg, mapper=DatasetMapperIgnore(cfg, is_train=True))
+        return build_detection_train_loader(cfg, mapper=DatasetMapperIgnore(cfg, is_train=True))
+
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    if args.opts:
+        cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    set_global_cfg(cfg)
+    default_setup(cfg, args)
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        return res
+
+    if cfg.QUERY_EXPAND.ENABLED:
+        if 'voc' in cfg.DATASETS.TRAIN[0]:
+            from fsdet.data.utils import register_results_voc
+            register_results_voc(cfg)
+        else:
+            from fsdet.data.utils import register_results
+            register_results(cfg)
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop or subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
